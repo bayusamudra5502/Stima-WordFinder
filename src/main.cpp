f7751bd9cf1be2affdf634815fefc98a1c987306
bayusamudra5502 <contact@bayusamudra.my.id>
2022-01-25 19:20:43 +0700

    Dan telah berakhir sudah tucil ini

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 
+#include "lib/Matcher.hpp"
 #include "lib/io/Console.hpp"
+#include "lib/io/File.hpp"
 using namespace std;
+using namespace FileManagement;
 
 int main() {
   Console::printHeader();
+  cout << endl;
+
+  string filename;
+  cout << "Insert file path : ";
+  getline(cin, filename);
+
+  FileReader reader(filename);
+  reader.parse();
+
+  vector<string> table = reader.getTable();
+  vector<string> patterns = reader.getPatterns();
+
+  Matcher::StringMatcher matcher(table);
+
+  for (string i : patterns) {
+    matcher.match(i);
+  }
+
+  Console::printTable(table, matcher.getColorizedResult());
 
   return 0;
 }
